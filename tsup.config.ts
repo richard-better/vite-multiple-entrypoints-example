44d18ca0a03b7a0f1c861d56a637f1f604f59fa5
Richard Borcsik <git@richardb.me>
2023-02-27 21:18:11 +0100

    add tsup example

---
+++ tsup.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'tsup';
+
+import pkgJson from './package.json';
+
+export const tsup = defineConfig({
+  entry: ['./src/components/markdown-article/article.tsx', './src/components/rich-text-article/article.tsx'],
+  splitting: true,
+  sourcemap: true,
+  clean: true,
+  dts: {},
+  format: ['esm', 'cjs'],
+  legacyOutput: false,
+  treeshake: true,
+});
